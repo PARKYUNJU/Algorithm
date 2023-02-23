59bbf63bdced884b3de61b41a09f7bf8f87e0437
PARKYUNJU <86733856+PARKYUNJU@users.noreply.github.com>
2023-02-23 20:55:03 +0900

    [Silver V] Title: 수들의 합 5, Time: 148 ms, Memory: 12880 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
@@ -4,7 +4,7 @@ import java.io.*;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-      Scanner sc=new Scanner(System.in);
+             Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
 
         int count=1;
@@ -15,8 +15,6 @@ public class Main {
 
         while(end!=n)
         {
-//            System.out.println("현재 start"+start);
-//            System.out.println("현재 end"+end);s
 
             if(sum==n)
             {
@@ -26,13 +24,12 @@ public class Main {
             }
             else if(sum>n)
             {
-                sum-=start;
-
+                sum-=start; //부분합을 구하는 과정. 현재까지 누적합 - 앞에 인덱스 (중요*****)
                 start++;
             }
             else {
                 end++;
-                sum+=end;
+                sum+=end; //굳이 부분합을 구하지 않아도 인덱스를 sum에 더해가는게 누적합임.
 
             }
 
@@ -42,5 +39,11 @@ public class Main {
         }
 
         System.out.println(count);
+
+
+        /* 처음 짰던 코드의 문제점
+        부분합을 subSum(int a, int b)라고 따로 외부 함수를 만들어서 호출하고,
+        메인 함수에서는 while(true)로 무한루프 돌려놓고 end가 끝까지 가면 break 하는식으로 했는데 while(end!=n)이랑 시간복잡도 차이가 있는지 궁금하다
+         */
     }
 }
