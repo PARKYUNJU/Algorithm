995be30b8ca0c3e4b3686e923063b76a7e4c2c3f
PARKYUNJU <86733856+PARKYUNJU@users.noreply.github.com>
2023-03-20 21:19:02 +0900

    [level 1] Title: 폰켓몬, Time: 2.05 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] nums) {
+       int choice=nums.length/2;
+        int result=0;
+        Map<Integer,Integer> map=new HashMap<>();
+
+        for(int i=0;i<nums.length;i++)
+        {
+            if(!map.containsValue(nums[i]))
+                map.put(i,nums[i]);
+        }
+
+        int size=map.size();
+        if(size>choice)
+            result=choice;
+        else
+            result=size;
+        
+        
+        return result;
+    }
+}
